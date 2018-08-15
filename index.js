9ab5d4317bcbaeecd3ae81a27223102e1df2c8f7
Abdel Elbouhy <abdel@Abdels-MacBook-Pro.local>
2018-08-15 10:28:21 +0100

    Add handlebars

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ import Router from 'koa-router';
 import graphqlHTTP from 'koa-graphql';
 import mongoose from 'mongoose';
 import schema from './schema';
+import serve from 'koa-static';
+import views from 'koa-views';
+import path from 'path';
 
 const app = new Koa();
 const router = new Router();
+
+app.use(views(path.resolve(__dirname, './views'), {
+    map: {
+        hbs: 'handlebars',
+    },
+    extension: 'hbs',
+    options: {
+        partials: {
+            Header: './partials/header',
+        }
+    }
+}));
+
+app.use(serve('./images'));
 
 mongoose.connect('mongodb://abdelelbouhy:1031972a@ds257851.mlab.com:57851/abdeltest', {useNewUrlParser: true});
 mongoose.connection.once('open', () => console.log('mongoose connected'));
@@ -14,6 +31,14 @@ router.all('/graphql', graphqlHTTP({
     schema,
     graphiql: true,
 }));
+
+router.get('/test', async (ctx) => {
+    await ctx.render('index', {
+        title: 'London School of Digital Marketing | Home',
+        header: 'this is header'
+    });
+});
+
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => console.log('Running a GraphQL API server at localhost:4000/graphql'));
